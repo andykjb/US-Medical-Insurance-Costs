5c2f61a4dabf489e624c66154ca95727f10468d0
andykjb <84149105+andykjb@users.noreply.github.com>
2021-07-18 23:08:38 +0200

    working on age mean()

--- medical insurance costs.py
+++ medical insurance costs.py
@@ -28,7 +28,59 @@
 # Ratio of BMI low, normal, high
 
 
-import pandas
+import pandas as pd
 
-import
+
+insurance_data = pd.read_csv('insurance.csv')
+
+
+
+
+## Age ##
+#Average age, for all and gender
+avg_age = insurance_data['age'].mean()
+#prints 39.2
+
+sum_f_age = 0
+num_of_females = 0
+if insurance_data['sex'] == 'female':
+    sum_f_age += int(insurance_data['age'])
+    num_of_females += 1
+
+avg_age_female = sum_f_age / num_of_females
+print(avg_age_female)
+
+
+## Charges ##
+# Average charge, for all and gender
+avg_charge = insurance_data['charges'].mean()
+
+
+
+## Smoking ##
+# Find ratio of smokers in genders
+
+
+# Find ratio of smokers with low, normal and high BMI
+# Find ratio of smokers for people with and without kids
+# Average price increanse for smokers vs non-smokers
+
+## Regions ##
+# Find ratio between regions
+# Find price difference between regions
+# Possible reason for higher, lower price in certain region
+
+## Children ##
+# Ratio of having kids vs not
+# Average price increase of 1 child
+# Average amount of kids in segment 'having kids'
+
+## BMI ##
+# Average BMI
+# Average price increase for high BMI
+# Ratio of BMI low, normal, high
+
+
+
+
 
