e63249800947534d7e5d526557b518f4ecfc7a59
andykjb <84149105+andykjb@users.noreply.github.com>
2021-07-18 22:20:23 +0200

    Add files via upload

---
+++ us-medical insurance costs.py
@@ -0,0 +1,31 @@
+### Scope ###
+
+## Age ##
+# Find the average age, for all and gender
+
+## Charges ##
+# Average charge, for all and gender
+
+## Smoking ##
+# Find ratio of smokers in genders
+# Find ratio of smokers with low, normal and high BMI
+# Find ratio of smokers for people with and without kids
+# Average price increanse for smokers vs non-smokers
+
+## Regions ##
+# Find ratio between regions
+# Find price difference between regions
+# Possible reason for higher, lower price in certain region
+
+## Children ##
+# Ratio of having kids vs not
+# Average price increase of 1 child
+# Average amount of kids in segment 'having kids'
+
+## BMI ##
+# Average BMI
+# Average price increase for high BMI
+# Ratio of BMI low, normal, high
+
+
+import pandas
